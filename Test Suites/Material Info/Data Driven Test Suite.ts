6358ed3534de00692031fbb6c8b8255134a8fd40
BaPpY <bappyasif@hotmail.com>
2019-10-02 22:10:13 +0600

    Initial Commit.
    Data Binding Example Project.

---
+++ Test Suites/Material Info/Data Driven Test Suite.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Using Excel File As Input Strem For Test Steps.</description>
+   <name>Data Driven Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a0da8c3c-b108-49d6-ae60-bf83d1f1b240</testSuiteGuid>
+   <testCaseLink>
+      <guid>6aea4628-068b-4af0-89a8-776d8730c3b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Recordings/User Login Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>adba62ef-ae96-4560-917b-7f28e66e436b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User Profile Test Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>adba62ef-ae96-4560-917b-7f28e66e436b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>User Email</value>
+         <variableId>b4549f0d-9e5b-479b-97cc-c44b8025f164</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>adba62ef-ae96-4560-917b-7f28e66e436b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>User Password</value>
+         <variableId>7e6c6343-618b-428f-8fa0-3b9c3d36831c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
