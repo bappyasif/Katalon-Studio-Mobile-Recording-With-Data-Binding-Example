f9c1fcf593b6bb876004c68218b84388e7722a8e
BaPpY <bappyasif@hotmail.com>
2019-10-03 22:15:52 +0600

    Added To Last Commit.
    User Creation For Material Info App Using Data Binding.

---
+++ Test Suites/Material User/Creating User Test Suite.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>With Data-Binding And Exporting Result To Katalon Analytics.</description>
+   <name>Creating User Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a754e36d-29f3-4f53-bef1-2a48a4b293c8</testSuiteGuid>
+   <testCaseLink>
+      <guid>038b6180-a525-4ae9-a46f-686ebf57481d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Recordings/Create User Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Material User Test Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Name</value>
+         <variableId>dec06aa7-13ca-4583-8457-763ba6bb18ca</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Address</value>
+         <variableId>d698ac5e-8395-4a4d-8d30-125543067f46</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>8955b564-809c-4a25-8c4c-9cb8694680cc</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Mobile Number</value>
+         <variableId>d78402ff-5673-462c-8716-4631d50b9444</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>fde7f69b-8c26-4fa1-a107-a5ce7cb6d9d3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>edb41d5a-7f0f-4260-83ed-b3150dd03c4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Re-Enter Password</value>
+         <variableId>5a6271eb-09fe-442a-875d-30111f463731</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
